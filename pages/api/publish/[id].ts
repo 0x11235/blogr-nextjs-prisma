8f0436c6ccbdffddbe8bfa77e4a13b5b837f90db
Megalodon <37167612+0xmegalodon@users.noreply.github.com>
2026-01-29 21:55:32 +0700

    add delete post

---
+++ pages/api/publish/[id].ts
@@ -0,0 +1,15 @@
+import prisma from "../../../lib/prisma";
+
+// PUT /api/publish/:id
+export default async function handle(req, res) {
+    const postId = req.query.id;
+    const post = await prisma.post.update({
+        where: {
+            id: String(postId),
+        },
+        data: {
+            published: true,
+        },
+    });
+    res.json(post);
+}
